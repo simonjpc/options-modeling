c87edc4f26ec5b93bc39ac8c3881cfb7906020f4
Simon Palma <simon@MacBook-Pro-de-Simon.local>
2025-08-28 12:28:27 +0200

    gitignore update and setup.py adding

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="options",
+    packages=find_packages(include=["exp1", "exp2"]),
+    version="0.1.0",
+    description="options trading exploration",
+    author="Simon Palma",
+    license="",
+)
